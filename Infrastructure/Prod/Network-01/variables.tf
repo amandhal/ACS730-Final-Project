05d60b8b167aaa61a3b75b4a222cb4aa9ffcbdd1
safeerahmad5900 <safeerahmad5900@gmail.com>
2022-12-09 06:53:13 +0000

    Adding Cidr ranges for Prod Env

--- Infrastructure/Prod/Network-01/variables.tf
+++ Infrastructure/Prod/Network-01/variables.tf
@@ -31,3 +31,16 @@ variable "env" {
 }
 
 
+# Provision private subnets in custom VPC
+variable "private_subnet_cidrs" {
+  default     = ["10.250.1.0/24", "10.250.2.0/24", "10.250.3.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs"
+}
+
+# Provision public subnets in custom VPC
+variable "public_subnet_cidrs" {
+  default     = ["10.250.4.0/24", "10.250.5.0/24", "10.250.6.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
