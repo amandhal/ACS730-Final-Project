40789d7992fcc9db5dabf9e611e80f78efa70873
asingla7git <asingla7@myseneca.ca>
2022-12-09 02:39:47 +0000

    Adding basic network for staging in Network-01

--- Infrastructure/Dev/Network-01/main.tf
+++ Infrastructure/Dev/Network-01/main.tf
@@ -2,3 +2,15 @@
 module "globalvars" {
   source = "../../../Modules/globalvars"
 }
+
+# module to deploy basic networing for Staging  
+module "staging-networing" {
+  source              = "../../../Modules/Netowrk-G05"
+  env                 = var.env
+  vpc_cidr            = var.vpc_cidr
+  private_cidr_blocks = var.private_subnet_cidrs
+  public_cidr_blocks  = var.public_subnet_cidrs
+  prefix              = module.globalvars.prefix
+  default_tags        = module.globalvars.default_tags
+
+}
