dde095f93ea49ead460fb28205559137ae4d3add
amishdungrani21 <amishdungrani21@gmail.com>
2022-12-09 04:54:44 +0000

    Adding variable instance type and env

---
+++ Infrastructure/Dev/WebServer-01/variables.tf
@@ -0,0 +1,27 @@
+/*# Default tags
+variable "default_tags" {
+  default     = {}
+  type        = map(any)
+  description = "Default tags to be appliad to all AWS resources"
+}
+
+# Name prefix Ref from Globalvars
+variable "prefix" {
+  type        = string
+  description = "Default Prefix Name Final Project G5"
+}
+*/
+# Instance type
+variable "instance_type" {
+  default     = "t3.micro"
+  description = "Type of the instance"
+  type        = string
+}
+
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "dev"
+  type        = string
+  description = "Deployment Environment"
+}
