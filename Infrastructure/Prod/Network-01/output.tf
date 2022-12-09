67fb75558d9fff9ee70f9efc68850e53496c9354
safeerahmad5900 <safeerahmad5900@gmail.com>
2022-12-09 07:29:26 +0000

    Output File Update

--- Infrastructure/Prod/Network-01/output.tf
+++ Infrastructure/Prod/Network-01/output.tf
@@ -25,3 +25,26 @@ output "aws_eip" {
   value = module.prod-networing.aws_eip
 }
 
+# Output variables for Private Route
+
+output "private_route_table" {
+  value = module.staging-networing.private_route_table
+}
+
+# Output variables for Public Route Table
+
+output "public_route_table" {
+  value = module.staging-networing.public_route_table
+}
+
+# Output variables for Public Cidr Block
+output "public_cidr_blocks" {
+  value = module.staging-networing.public_cidr_blocks
+}
+
+# Output variables for Private CIDR block 
+output "private_cidr_blocks" {
+  value = module.staging-networing.private_cidr_blocks
+}
+
+
