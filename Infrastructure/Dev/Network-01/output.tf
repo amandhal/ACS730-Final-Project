2afae2e52c33759ec818c18a23a8d3adee358ecb
asingla7git <asingla7@myseneca.ca>
2022-12-09 04:06:14 +0000

    Adding outputs of route tables, cidr blocks and elastic ip

--- Infrastructure/Dev/Network-01/output.tf
+++ Infrastructure/Dev/Network-01/output.tf
@@ -17,3 +17,31 @@ output "private_subnet_ids" {
 output "aws_internet_gateway" {
   value = module.staging-networing.aws_internet_gateway
 }
+
+# Output variables for Elastic IP
+
+output "aws_eip" {
+  value = module.staging-networing.aws_eip
+}
+
+# Output variables for Private Route
+
+output "private_route_table" {
+  value = module.staging-networing.private_route_table
+}
+
+# Output variables for Public Route Table
+
+output "public_route_table" {
+  value = module.staging-networing.public_route_table
+}
+
+# Output variables for Public Cidr Block
+output "public_cidr_blocks" {
+  value = module.staging-networing.public_cidr_blocks
+}
+
+# Output variables for Private CIDR block 
+output "private_cidr_blocks" {
+  value = module.staging-networing.private_cidr_blocks
+}
