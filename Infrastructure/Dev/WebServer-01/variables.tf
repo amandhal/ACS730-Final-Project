570da0caee6df84f08a57a864b68d5fec4117975
amishdungrani21 <amishdungrani21@gmail.com>
2022-12-09 05:08:11 +0000

    Giving path of linux key and setting min & max group size for auto scaling and desired capacity as well

--- Infrastructure/Dev/WebServer-01/variables.tf
+++ Infrastructure/Dev/WebServer-01/variables.tf
@@ -25,3 +25,29 @@ variable "env" {
   type        = string
   description = "Deployment Environment"
 }
+
+variable "path_to_linux_key" {
+  default     = "/home/ec2-user/.ssh/Group-No-5-dev.pub"
+  description = "Path to the public key to use in Linux VMs provisioning"
+  type        = string
+}
+
+#Desired Capacity 
+variable "desired_capacity" {
+  default     = 3
+  type        = number
+  description = "Optimal Capacity"
+}
+
+#Minimum group size for auto scaling
+variable "minimum_size" {
+  default     = 1
+  type        = number
+  description = "Minimum group size for auto scaling"
+}
+#Maximum group size for auto scaling
+variable "maximum_size" {
+  default     = 4
+  type        = number
+  description = "Maximum group size for auto scaling"
+}
