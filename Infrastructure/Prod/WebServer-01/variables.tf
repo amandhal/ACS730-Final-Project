674714c24a815cf42348d1850dc0994b8a139b65
safeerahmad5900 <safeerahmad5900@gmail.com>
2022-12-09 08:16:29 +0000

    variables

--- Infrastructure/Prod/WebServer-01/variables.tf
+++ Infrastructure/Prod/WebServer-01/variables.tf
@@ -26,8 +26,6 @@ variable "env" {
   description = "Deployment Environment"
 }
 
-
-
 #Desired Capacity 
 variable "desired_capacity" {
   default     = 3
@@ -46,6 +44,28 @@ variable "maximum_size" {
   default     = 4
   type        = number
   description = "Maximum group size for auto scaling"
+}
+
+
+#Private IP of Cloud 9 Environment these values need to hardcoded 
+variable "my_private_ip" {
+  default     = "172.31.45.63"
+  type        = string
+  description = "Private IP of Cloud 9 Environment"
+}
+
+#Public IP of Cloud 9 Environment these values need to hardcoded 
+variable "my_public_ip" {
+  default     = "34.226.222.38"
+  type        = string
+  description = "Public IP of Cloud 9 Environment"
+}
+
+
+variable "path_to_linux_key" {
+  default     = "/home/ec2-user/.ssh/Group-No-5-prod.pub"
+  description = "Path to the public key to use in Linux VMs provisioning"
+  type        = string
 }
 
 
